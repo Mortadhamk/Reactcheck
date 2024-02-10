43913e32facdebd17aa888c82ef6489c4c8c7482
Mortadhamk <morta86mabrouk@gmail.com>
2024-02-10 14:56:01 +0100

    first commit

---
+++ reactt/src/App.js
@@ -0,0 +1,32 @@
+import React from'react';
+import'bootstrap/dist/css/bootstrap.min.css';
+// Import Bootstrap CSS
+
+
+function App(){
+    return(      
+<div>
+<div class="card-body">   
+<h5 class="card-title">Card1</h5>
+<p class="card-text">Somequick example text to build on d's content.</p>   
+<a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+
+<div class="card-body">  
+<h5 class="card-title">Card2</h5>
+<p class="card-text">Some hi ho dfdfza card's content.</p>   
+<a href="#"class="btn btn-primary">Go somewhere</a>
+</div>
+
+<div class="card-body">  
+<h5 class="card-title">Card3</h5>  
+<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> 
+<a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+
+
+</div>
+        );
+}
+
+export default App;
